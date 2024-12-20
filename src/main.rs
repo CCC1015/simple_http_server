9ced7ca3fa787fdde62d766a2eda22f7d6481f2d
CCC1015 <dylanojansen2303@gmail.com>
2024-12-20 15:44:55 +0100

    f

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
-use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
 use std::convert::Infallible;
+use std::net::SocketAddr;
 
+// Define the service function
 async fn handle_request(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    // Respond with "Hello, World!"
     Ok(Response::new(Body::from("Hello, World!")))
 }
 
 #[tokio::main]
 async fn main() {
-    // The address to bind to
-    let addr = ([127, 0, 0, 1], 3000).into();
+    // Define the address to bind the server to
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    // Create a service to handle requests
-    let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(handle_request))
+    // Create a make_service_fn
+    let make_svc = make_service_fn(|_conn| {
+        async { Ok::<_, Infallible>(service_fn(handle_request)) }
     });
 
-    // Create and bind the server
+    // Bind the server to the address and serve the make_svc
     let server = Server::bind(&addr).serve(make_svc);
-
-    println!("Server running on http://{}", addr);
 
     // Run the server
     if let Err(e) = server.await {
